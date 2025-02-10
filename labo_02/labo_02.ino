3ccec01e444b79be2d2c7d95da98aa0accba269c
Nick B <nbourrerg@hotmail.com>
2025-02-10 14:27:43 -0500

    Premier commit

---
+++ labo_02/labo_02.ino
@@ -0,0 +1,16 @@
+unsigned long currentTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  // Ton code ici
+
+
+
+  Serial.println("Setup completed");
+}
+
+void loop() {
+  currentTime = millis();
+
+}
